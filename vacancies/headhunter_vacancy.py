af3263ff8a5d6e80fd98eed179e7d49e9ac81b63
Petr Davydov <Davydov.atc@yandex.ru>
2023-06-22 14:20:48 +0300

    Initial commit

---
+++ vacancies/headhunter_vacancy.py
@@ -0,0 +1,8 @@
+from vacancies.vacancy_base_class import VacancyBaseClass
+
+
+class HeadHunterVacancy(VacancyBaseClass):
+    """
+    Класс вакансий с HeadHunter
+    """
+    pass
---
+++ vacancies/superjob_vacancy.py
@@ -0,0 +1,8 @@
+from vacancies.vacancy_base_class import VacancyBaseClass
+
+
+class SuperJobVacancy(VacancyBaseClass):
+    """
+    Класс вакансии SuperJob
+    """
+    pass
